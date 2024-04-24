344547106ba87191b03830b9b58b36031cfdfd03
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-24 18:57:46 +0700

    displaying the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PhotosComponent } from './components/photos/photos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PhotosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'photo-fetching-app';
