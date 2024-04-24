11f2ba8980b724ece10a1145bc7913da210dfb8f
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-24 19:03:27 +0700

    making a request

--- src/app/components/photos/photos.component.ts
+++ src/app/components/photos/photos.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { PhotosService } from '../../photos.service';
 
 @Component({
   selector: 'app-photos',
   standalone: true,
   imports: [],
   templateUrl: './photos.component.html',
-  styleUrl: './photos.component.css'
+  styleUrl: './photos.component.css',
 })
 export class PhotosComponent {
-
+  constructor(private photosService: PhotosService) {
+    this.photosService.getPhoto().subscribe(() => {});
+  }
 }
