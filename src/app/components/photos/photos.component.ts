3011f0d0286d48678c7e4277c77b906540ee2140
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-24 19:17:41 +0700

    refetching data

--- src/app/components/photos/photos.component.ts
+++ src/app/components/photos/photos.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { PhotosService } from '../../photos.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-photos',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './photos.component.html',
   styleUrl: './photos.component.css',
 })
 export class PhotosComponent {
   photoUrl!: string;
+
   constructor(private photosService: PhotosService) {
+    this.fetchPhoto();
+  }
+
+  onClick() {
+    this.fetchPhoto();
+  }
+
+  fetchPhoto() {
     this.photosService.getPhoto().subscribe((res) => {
       this.photoUrl = res.urls.regular;
     });
